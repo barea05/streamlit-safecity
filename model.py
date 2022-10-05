89f2c59111ae9fb4651c6ad458b9b29f4a3c4c32
DHIVAKAR <dave.barea@gmail.com>
2022-10-05 21:37:46 +0530

    make it better

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ with open("data\Ensemble_models/Random_forest_bow.pkl", "rb") as pickle_file:
 with open("data\Ensemble_models/Random_forest_tfidf.pkl", "rb") as pickle_file:
     Random_forest_tfidf = pickle.load(pickle_file)
 
-with open("D:\Main thesis\safecity-master\SafeCity_Main Project\Best_model/xgboost_bow.pkl", "rb") as pickle_file:
+with open("data\Ensemble_models/xgboost_bow.pkl", "rb") as pickle_file:
     xgboost_bow = pickle.load(pickle_file)
 
 with open("data\\Ensemble_models\\xgboost_tfidf.pkl", "rb") as pickle_file:
